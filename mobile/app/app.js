7d1b2c927f25c347ff613a2aefdf678e9aa87805
Christophe Mollaret <christophe.mollaret.pro@gmail.com>
2022-01-14 07:42:42 +0100

    setted up vuex

--- mobile/app/app.js
+++ mobile/app/app.js
@@ -1,8 +1,12 @@
 import Vue from "nativescript-vue";
 import { localize, androidLaunchEventLocalizationHandler } from '@nativescript/localize';
 import { on, launchEvent } from '@nativescript/core/application';
+import Vuex from 'vuex';
+Vue.use(Vuex);
 
 import Login from "./pages/Login";
+
+import store from './store/main';
 
 Vue.config.silent = false
 Vue.filter("L", localize);
@@ -15,4 +19,5 @@ on(launchEvent, (args) => {
 
 new Vue({
     render: h => h('frame', [h(Login)]),
+    store: store
 }).$start();
---
+++ mobile/app/store/main.js
@@ -0,0 +1,13 @@
+import Vue from 'nativescript-vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    lastMessage: ""
+  },
+  mutations: {
+  }
+});
+
+export default store;
